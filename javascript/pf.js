1553b54f89f51365e4a2947597847c78b993919e
Vishnu Vardhan <vishnuvardhan15503@gmail.com>
2023-08-08 10:43:40 +0530

    Add toggle functionality.

--- javascript/pf.js
+++ javascript/pf.js
@@ -14,3 +14,23 @@ $(document).ready(function(){
 
 
 });
+
+const btn1 = document.getElementById('sgc');
+const btn2 = document.getElementById('ragam');
+
+const content_1 = document.getElementById('sgc-content');
+const content_2 = document.getElementById('ragam-content');
+
+btn1.addEventListener('click', () => {
+    btn1.classList.add('clicked');
+    btn2.classList.remove('clicked');
+    content_1.classList.remove('hidden');
+    content_2.classList.add('hidden');
+});
+
+btn2.addEventListener('click', () => {
+    btn2.classList.add('clicked');
+    btn1.classList.remove('clicked');
+    content_2.classList.remove('hidden');
+    content_1.classList.add('hidden');
+});
